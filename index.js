431b75240fbcc987a030e153f8176859fe4c9c62
Joanna Preston <joannappreston@gmail.com>
2020-04-27 10:32:55 -0700

    prompts user for input using inquirer

--- index.js
+++ index.js
@@ -10,3 +10,51 @@ const inquirer = require("inquirer");
 const util = require("util");
 const axios = require("axios");
 
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Please enter your project title: "
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please enter a description of your project: "
+        },
+        {
+            type: "input",
+            name: "installInstruct",
+            message: "Please enter your installation instructions here: "
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How do you use this product? "
+        },
+        {
+            type: "input",
+            name: "contributors",
+            message: "Who contributed to this project? "
+        },
+        {
+            type: "input",
+            name: "installInstruct",
+            message: "Please enter your installation instructions here: "
+        },
+        // still need: table of contents, badge?, license?, tests?, questions?, gitHub pic, gitHub email
+    ])
+    .then(function(data){
+        console.log(data);
+
+    })
+    .catch(function(err) {
+        if(err) {
+            console.log(err);
+        };
+    });
