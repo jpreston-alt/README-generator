8e4d942129a40f88bff856287c03f802738246af
Joanna Preston <joannappreston@gmail.com>
2020-04-27 10:08:44 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// initialize npm
+// install dependencies: inquirer, fs, util, axios
+// use util to create promises (writeFile)
+// inquirer - prompt user: github name, badge, project title, description, table of contents, installation, usage, license, contributing, tests, questions, github profile picture, github email
+// axios - get github picture and email with inputed github username
+// generate md file with user input
