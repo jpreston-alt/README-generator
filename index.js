a668d0eafbf60474d45fcd01c51b22eada0699f6
Joanna Preston <joannappreston@gmail.com>
2020-04-27 10:19:47 -0700

    initialied node repo. installed and required depedencies. created gitignore file and included node_modules

--- index.js
+++ index.js
@@ -4,3 +4,9 @@
 // inquirer - prompt user: github name, badge, project title, description, table of contents, installation, usage, license, contributing, tests, questions, github profile picture, github email
 // axios - get github picture and email with inputed github username
 // generate md file with user input
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const axios = require("axios");
+
