3f381614bbe82e4380b08d27bbe8f9f3b75e588f
Joanna Preston <joannappreston@gmail.com>
2020-04-30 13:28:40 -0700

    cleaned up spacing

--- README.md
+++ README.md
@@ -1,41 +1,53 @@
-# README-generator
-![GitHub last commit](https://img.shields.io/github/last-commit/jpreston-alt/README-generator) [![Link to Repo](https://img.shields.io/badge/Link%20to%20Repo-blue.svg)](https://github.com/jpreston-alt/README-generator)
-
+# README-Generator
+![GitHub last commit](https://img.shields.io/github/last-commit/jpreston-alt/README-Generator) [![Link to Repo](https://img.shields.io/badge/Link%20to%20Repo-blue.svg)](https://github.com/jpreston-alt/README-Generator)
 
 ## Description
-et
-
+Generate a README based on user input
 ## Table of Contents
 * [Usage](#usage)
 * [Technologies](#technologies)
 * [Questions](#questions)
 * [Installation](#Installation)
-
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [License](#License)
 
 ## Usage
-test
+answer the questions
 
-![](test)
-
+![](./assets/code-quiz.gif)
 ## Technologies
-* HTML
+* JavaScript
+* Node.js
 
-![GitHub top language](https://img.shields.io/github/languages/top/jpreston-alt/README-generator)
-
+![GitHub top language](https://img.shields.io/github/languages/top/jpreston-alt/README-Generator)
 
 ## Installation
 To install necessary dependencies, run the following command:
 ```
-test
+npm install
 ```
-![Dependencies Shield](https://img.shields.io/david/jpreston-alt/README-generator)
+![Dependencies Shield](https://img.shields.io/david/jpreston-alt/README-Generator)
 
+## License
+This project is licensed under the MIT license. 
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Contribution
+hit me up
+
+## Tests
+To run tests, run the following command: 
+``` 
+npm test 
+```
 
 
 ## Questions
 ​
 If you have any questions about the repo, open an issue or contact me:
 
-On GitHub: [jpreston-alt](https://api.github.com/users/jpreston-alt) | Via Email: joannappreston
+On GitHub: [jpreston-alt](https://api.github.com/users/jpreston-alt) | Via Email: joannappreston@gmail.com
 
 ![user image](https://avatars1.githubusercontent.com/u/58855401?v=4&s=100)
--- index.js
+++ index.js
@@ -9,167 +9,153 @@ const followUpMod = require("./followUpMod");
 // create promises
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-
 let usernameQ = {
     type: "input",
     name: "username",
     message: "What is your GitHub username? "
-}
+};
+
+let emailQ = {
+    type: "input",
+    name: "email",
+    message: "What is your email? "
+};
+
+let projectTitleQ = {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the name of your project repository? "
+};
+
+let descriptionQ = {
+    type: "input",
+    name: "description",
+    message: "Please write a description of your project: "
+};
+
+let usageQ = {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo? "
+};
+
+let imageQ = {
+    type: "input",
+    name: "projectIMG",
+    message: "Please include an image or a gif. Enter the filepath here: "
+};
+
+let technologiesQ = {
+    type: 'checkbox',
+    message: 'Which technologies did you use?',
+    name: 'technologies',
+    choices: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "Bulma", "Node.js", "AJAX"]
+};
+
+let extraSectionsQ = {
+    type: 'checkbox',
+    message: "What other sections would you like to include in your README? (Press ENTER if you don't want to include any)",
+    name: 'extraSections',
+    choices: ["Installation", "Contributing", "Tests", "License"],
+};
+
+const secondQ = [emailQ, projectTitleQ, descriptionQ, usageQ, imageQ, technologiesQ, extraSectionsQ];
 
 // prompt first set of questions
-function getValidUsername() {
+function init() {
     inquirer.prompt(usernameQ).then(function (ans) {
-        // console.log(ans.username);
-        axios
-            .get(`https://api.github.com/users/${ans.username}`)
-            .then(function (response) {
-                    const { username } = ans;
-                inquirer
-                    .prompt([
+        axios.get(`https://api.github.com/users/${ans.username}`).then(function (response) {
+            const { username } = ans;
+            const imageURL = `${response.data.avatar_url}&s=100`;
+            const profileURL = response.data.url;
 
-                        {
-                            type: "input",
-                            name: "email",
-                            message: "What is your email? "
-                        },
-                        {
-                            type: "input",
-                            name: "projectTitle",
-                            message: "What is the name of your project repository? "
-                        },
-                        {
-                            type: "input",
-                            name: "description",
-                            message: "Please write a description of your project: "
-                        },
-                        {
-                            type: "input",
-                            name: "usage",
-                            message: "What does the user need to know about using the repo? "
-                        },
-                        {
-                            type: "input",
-                            name: "projectIMG",
-                            message: "Please include an image or a gif. Enter the filepath here: "
-                        },
-                        {
-                            type: 'checkbox',
-                            message: 'Which technologies did you use?',
-                            name: 'technologies',
-                            choices: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "Bulma", "Node.js", "AJAX"]
-                        },
-                        {
-                            type: 'checkbox',
-                            message: "What other sections would you like to include in your README? (Press ENTER if you don't want to include any)",
-                            name: 'extraSections',
-                            choices: ["Installation", "Contributing", "Tests", "License"],
-                        }
-                    ])
-                    .then(function (data) {
+            inquirer.prompt(secondQ).then(function (data) {
 
-                        // desctrucure data object into variables
-                        const {projectTitle,
-                            description,
-                            usage,
-                            technologies,
-                            extraSections,
-                            email,
-                            projectIMG } = data;
+                // desctrucure data object into variables
+                const {projectTitle,
+                    description,
+                    usage,
+                    technologies,
+                    extraSections,
+                    email,
+                    projectIMG } = data;
 
-                        // questions array is generated based on users answer to extraSections
-                        const questionsArr = followUpMod.extraQarr(extraSections);
+                // questions array is generated based on users answer to extraSections
+                const questionsArr = followUpMod.extraQarr(extraSections);
 
                         // prompt user for answers to questions array
-                        inquirer
-                            .prompt(questionsArr)
-                            .then(function (data) {
-                                const { license, contributing, test, install } = data;
+                inquirer.prompt(questionsArr).then(function (data) {
+                    const { license, contributing, test, install } = data;
 
-                                // new section instances - sections are rendered based on user picks
-                                const licenseSection = new followUpMod.Section("License", dedent(`This project is licensed under the ${license} license. \n\n ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`));
-                                const contributionSection = new followUpMod.Section("Contribution", contributing);
-                                const testSection = new followUpMod.Section("Tests", dedent(`To run tests, run the following command: \n \`\`\` \n ${test} \n \`\`\` `));
-                                const installSection = new followUpMod.Section("Installation", dedent(`
-                        To install necessary dependencies, run the following command:
-                        \`\`\`
-                        ${install}
-                        \`\`\`
-                        ![Dependencies Shield](https://img.shields.io/david/${username}/${projectTitle})`))
+                    // new section instances - sections are rendered based on user picks
+                    const licenseSection = new followUpMod.Section("License", dedent(`This project is licensed under the ${license} license. \n\n ![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`));
+                    const contributionSection = new followUpMod.Section("Contribution", contributing);
+                    const testSection = new followUpMod.Section("Tests", dedent(`To run tests, run the following command: \n \`\`\` \n ${test} \n \`\`\` `));
+                    const installSection = new followUpMod.Section("Installation", dedent(`
+                    To install necessary dependencies, run the following command:
+                    \`\`\`
+                    ${install}
+                    \`\`\`
+                    ![Dependencies Shield](https://img.shields.io/david/${username}/${projectTitle})`))
 
-                                // push valid sections into new sections array
-                                let sectionsArr = [];
-                                installSection.validate(sectionsArr);
-                                licenseSection.validate(sectionsArr);
-                                contributionSection.validate(sectionsArr);
-                                testSection.validate(sectionsArr);
-                                // console.log(sectionsArr);
+                    // push valid sections into new sections array
+                    let sectionsArr = [];
+                    installSection.validate(sectionsArr);
+                    licenseSection.validate(sectionsArr);
+                    contributionSection.validate(sectionsArr);
+                    testSection.validate(sectionsArr);
+                    // console.log(sectionsArr);
 
-                                // render new sections, table of conetents, and technologies list
-                                let newSections = followUpMod.renderSections(sectionsArr);
-                                let toc = followUpMod.renderTOC(extraSections);
-                                let techsList = renderTechs(technologies);
+                    // render new sections, table of conetents, and technologies list
+                    let newSections = followUpMod.renderSections(sectionsArr);
+                    let toc = followUpMod.renderTOC(extraSections);
+                    let techsList = renderTechs(technologies);
 
-                                // use axios to generate user photo and link from gitHub with username
-                                axios
-                                    .get(`https://api.github.com/users/${username}`)
-                                    .then(function (response) {
-                                        const imageURL = `${response.data.avatar_url}&s=100`;
-                                        const profileURL = response.data.url;
-
-                                        const mdFile = dedent(`
-                        # ${projectTitle}
-                        ![GitHub last commit](https://img.shields.io/github/last-commit/${username}/${projectTitle}) [![Link to Repo](https://img.shields.io/badge/Link%20to%20Repo-blue.svg)](https://github.com/${username}/${projectTitle})
-                        
-
-                        ## Description
-                        ${description}
-
-                        ## Table of Contents
-                        * [Usage](#usage)
-                        * [Technologies](#technologies)
-                        * [Questions](#questions)
-                        ${toc}
-
-                        ## Usage
-                        ${usage}\n
-                        ![](${projectIMG})
-
-                        ## Technologies
-                        ${techsList} 
-                        ![GitHub top language](https://img.shields.io/github/languages/top/${username}/${projectTitle})\n
-
-                        ${newSections}
-
-                        ## Questions
+                    const mdFile = dedent(`
+                    # ${projectTitle}
+                    ![GitHub last commit](https://img.shields.io/github/last-commit/${username}/${projectTitle}) [![Link to Repo](https://img.shields.io/badge/Link%20to%20Repo-blue.svg)](https://github.com/${username}/${projectTitle})
+                    
+                    ## Description
+                    ${description}
+                    ## Table of Contents
+                    * [Usage](#usage)
+                    * [Technologies](#technologies)
+                    * [Questions](#questions)
+                    ${toc}
+                    ## Usage
+                    ${usage}\n
+                    ![](${projectIMG})
+                    ## Technologies
+                    ${techsList} 
+                    ![GitHub top language](https://img.shields.io/github/languages/top/${username}/${projectTitle})\n
+                    ${newSections}
+                    ## Questions
 ​
-                        If you have any questions about the repo, open an issue or contact me:\n
-                        On GitHub: [${username}](${profileURL}) | Via Email: ${email}\n
-                        ![user image](${imageURL})
-                        `);
+                    If you have any questions about the repo, open an issue or contact me:\n
+                    On GitHub: [${username}](${profileURL}) | Via Email: ${email}\n
+                    ![user image](${imageURL})
+                    `);
 
-                                        // generate MD file with user input
-                                        return writeFileAsync("README.md", mdFile);
-                                    });
-                            })
-                    })
-                    // catch all errors
-                    .catch(function (err) {
-                        if (err) {
-                            console.log(err);
-                        };
-                    });
-
-            }).catch(function(err) {
-                if (err) {
-                    console.log("please enter a valid username")
-                    getValidUsername();
-                }
+                    // generate MD file with user input
+                    return writeFileAsync("README.md", mdFile);
+                })
             })
+            .catch(function (err) {
+                if (err) {
+                    throw err;
+                };
+            })
+        })
+        .catch(function(err) {
+            if (err) {
+                console.log("please enter a valid username")
+                init();
+            }
+        })
     })
-}
+};
 
-getValidUsername();
+init();
 
 // renders technologies list
 function renderTechs(arr) {
